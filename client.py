e255bbf9def2df18a8bd2172d6d752d4ae3579f0
Erica Chen <38142465+MengshanChen@users.noreply.github.com>
2019-03-10 15:57:11 -0700

    Delete useless codes

--- client.py
+++ client.py
@@ -1,24 +1,5 @@
 import requests
 import os
- 
-def send_image_to_server(image_path):
-    
-    url = 'http://127.0.0.1:5000/'
-    test_url = url + 'upload'
-
-    image_filename = os.path.basename(image_path)
-    multipart_form_data = {
-        'file': (image_filename, open(image_path, 'rb'))
-    }
- 
-    response = requests.post(test_url,files=multipart_form_data)
- 
-    #print(response.text)
-    print(response.status_code)
-    print(response.headers['content-length'])  
-    print(response.headers['Content-Type'])  
-    print(response.headers['Date'])  
-
 
 def send_data_to_server(request, image_path):
     
@@ -43,7 +24,4 @@ def send_data_to_server(request, image_path):
     print('Date: ', response.headers['Date'])  
 
 if __name__ == '__main__':
-	#send_image_to_server("static/images/test.jpg")
     send_data_to_server("flip_h,rotate_360,grayscale,w_400,h_500,rotate_left", "static/images/test.jpg")
-
-#https://stackabuse.com/the-python-requests-module/
